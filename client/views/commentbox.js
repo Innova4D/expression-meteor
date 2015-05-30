21fce791dd2b2c9774f8fb20b7fe98c6e4f8ed6b
Francisco Gutierrez <fsalvador23@gmail.com>
2015-05-29 20:43:15 -0500

    Issue on reactivity session variable

--- client/views/commentbox.js
+++ client/views/commentbox.js
@@ -1,18 +1,14 @@
 Template.commentbox.events({
   "change .showPositives": function (event) {
-    console.log("positives");
     Session.set("showPositives", event.target.checked);
   },
   "change .showNeutrals": function (event) {
-    console.log("neutrals");
     Session.set("showNeutrals", event.target.checked);
   },
   "change .showNegatives": function (event) {
-    console.log("negatives");
     Session.set("showNegatives", event.target.checked);
   },
   "change .showAll": function (event) {
-    console.log("all");
     Session.set("showAll", event.target.checked);
   }
 });
@@ -20,26 +16,22 @@ Template.commentbox.events({
 Template.commentbox.helpers({
   comments: function () {
     if(Session.get("showPositives")) {
-      console.log("Positive-query");
       return Comments.find({topic: this.id, sentiment: { $gt: 0, $lt: 3 }});
     } else if (Session.get("showNeutrals")) {
-      console.log("neutral-query");
       return Comments.find({topic: this.id, sentiment: 0});
     } else if (Session.get("showNegatives")){
-      console.log("negative-query");
       return Comments.find({topic: this.id, sentiment: { $gt: -3, $lt: 0 }});
     } else {
-      console.log("all-query");
       return Comments.find({topic: this.id}, {sort: {posted:-1}});
     }
   },
   showPositives: function () {
-    return Session.get("showPositives");
+    return Session.set("showPositives", false);
   },
   showNegatives: function() {
-    return Session.get("showNegatives");
+    return Session.set("showNegatives", false);
   },
   showNeutrals: function() {
-    return Session.get("showNeutrals");
+    return Session.set("showNeutrals", false);
   }
 });
